754c6d70f3ae034b220406ff754d1ac7a42bf934
Jeremy Nelson <jnelson@dm.ai>
2019-01-30 13:00:53 -0800

    update

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f60c8e114cb2245598df935ad6334e6f.js"
+  "./precache-manifest.3e14b2bb274098d8e156c3bd9999e018.js"
 );
 
 workbox.clientsClaim();
